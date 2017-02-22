f997e5e6f02fa8fa6a293bc3360681ada0d93e36
Pavel Esir (lenovo) <esir.pavel@gmail.com>
2017-02-22 21:48:41 +0300

    Cleared version for final calculations

--- resultsPlot.py
+++ resultsPlot.py
@@ -18,11 +18,11 @@ N = 200
 Mm = 0.5
 
 Nreads = arange(dNread, N + 1, dNread, dtype='int')
-Urange = arange(0.05, 1.0, 0.05)
-Ierange = load('U_Iex_SimTime_20.0_h_0.0020_D_2.0_N_200_eps_0.010_m_{:.1f}.npy'.format(Mm))
+Urange = arange(0.05, 0.951, 0.05)
+#Ierange = load('U_Iex_SimTime_20.0_h_0.0020_D_2.0_N_200_eps_0.010_m_{:.1f}.npy'.format(Mm))
 
-folderName = 'res_h_0.0020_D_2.0_freq_{:.1f}_T_{:.2f}/'.format(freq, T)
-name = folderName + 'U_{:.2f}_C_{:.1f}_N_{:d}_SimTime_{:n}.npy'
+folderName = 'res_h_0.0020_D_2.0_freq_{:.1f}_T_{:.2f}_m_{:.1f}/'.format(freq, T, Mm)
+name = folderName + 'U_{:.2f}_C_{:.1f}_N_{:d}_SimTime_{:d}.npy'
 
 errR = np.load(name.format(Urange[0], C, N, SimTime))
 arrShape = len(Urange), shape(errR)[0], shape(errR)[1], shape(errR)[2]
@@ -34,27 +34,39 @@ for idx, u in enumerate(Urange):
 #%%
 
 meanErrR = mean(errR, axis=3)
-minLags = argmin(mean(errR, axis=3), axis=1)
+minLags = argmin(meanErrR, axis=1)
 minLagErr = amin(meanErrR, axis=1)
 
 minErrNread = zeros(len(Nreads))
-
-figure()
-title('$C={}$'.format(C))
+NreadsForPlot = [20, 40, 100, 200]
 for idx, Nread in enumerate(Nreads):
     errs = minLagErr[:, int(Nread/dNread - 1)]*360/(4*np.pi)
 
     minErrNread[idx] = Urange[argmin(errs)]
-    plot(Urange, errs, '-o', label=r'$N_{{read}}={}$'.format(Nread))
+    if Nread in NreadsForPlot:
+        figure(1, figsize=(4*2.5, 3*2.5))
+        plot(Urange, errs, '-o', label=r'$N_{{read}}={}$'.format(Nread))
+        
+        figure(2, figsize=(4*2.5, 3*2.5))
+        plot(Urange, minLags[:, int(Nread/dNread - 1)]*2., '-o', label=r'$N_{{read}}={}$'.format(Nread))
 
+figure(1, figsize=(4*2.5, 3*2.5))
+title('$C={}$'.format(C))
 legend(loc='upper right', fontsize=14.0)
 xlabel('U')
 ylabel('$Error[degree \; ^{\circ}$]')
+
+figure(2, figsize=(4*2.5, 3*2.5))
+title('$C={}$'.format(C))
+legend(loc='upper right', fontsize=14.0)
+xlabel('U')
+ylabel('$lag[ms]$')
 #savefig("Freq_{}_C_{}_Tsim_{}_mean_{}_N_{}.png".format(freq, C, SimTime, Mm, N), dpi=300.)
+
 #%%
-figure()
+figure(3, figsize=(4*2.5, 3*2.5))
 title(r"$C = {}$".format(C))
 plot(Nreads, minErrNread, '-o')
 xlabel(r'$N_{read}$')
 ylabel(r'U for minimal error')
-#savefig("U_for_min_Freq_{}_C_{}_Tsim_{}_mean_{}_N_{}.png".format(freq, C, SimTime, Mm, N), dpi=300.)
+savefig("U_for_min_Freq_{}_C_{}_Tsim_{}_mean_{}_N_{}.png".format(freq, C, SimTime, Mm, N), dpi=300.)
--- ring.py
+++ ring.py
@@ -18,39 +18,35 @@ seed(0)
 # can be used for parallel calculation with GNU parallel
 
 # if CalcMode is equal to 3 then I0 vs U curve is calculated by bisection method
-CalcMode = 0
+CalcMode = 2
 
 
-SimTime = 20.0        # seconds
+SimTime = 200.0        # seconds
 h = 0.002             # seconds
 pltSampl = 0.02       # variable save interval in s
 Tsim = int(SimTime/h)
 
 # to load empirical calculated U vs I0 dependence
 Mm = 0.5
-Urange = arange(0.05, 1.0, 0.05)
+Urange = arange(0.05, 0.951, 0.05)
 
 Ierange = [-1.170, -0.917, -0.669, -0.485, -0.485, -0.542, -0.711,
            -0.934, -1.154, -1.398, -1.690, -1.950, -2.256, -2.389,
            -2.543, -2.717, -2.855, -3.033, -3.150]
-#Ierange = load('U_Iex_SimTime_20.0_h_0.0020_D_2.0_N_200_eps_0.010_m_{:.1f}.npy'.format(Mm))
 #%%
 U = 0.25
 I0 = Ierange[int(U/0.05) - 1]
-#U = 0.0
-#I0 = Ierange[0]
 
 D = 2.0
 J0 = -12
 J1 = 30
 
 # duration of events in sec
-T = 1.00
+T = 0.05
 # amplitude of events
-#C = 20.0
 C = 10.0
 # input poisson events rate, Hz
-freq = 100
+freq = 8
 
 tau_r = 0.01
 
@@ -59,7 +55,8 @@ tau_n = 0.1
 tau_rec = 0.8
 N = 200
 
-folderName = 'res_h_0.0020_D_2.0_freq_{:.1f}_T_{:.2f}/'.format(freq, T)
+folderName = 'res_h_0.0020_D_{}_freq_{:.1f}_T_{:.2f}_m_{:.1f}/'.format(D, freq, T, Mm)
+#Ierange = load('U_Iex_SimTime_20.0_h_0.0020_D_{}_N_200_eps_0.010_m_{:.1f}.npy'.format(D, Mm))
 
 stime = arange(0, SimTime, h)
 m = zeros(N)
@@ -77,14 +74,14 @@ alpha, beta = meshgrid(th, th)
 W = (J0 + J1*cos(alpha - beta))/N
 del alpha, beta
 #%%
-# number of input events, nearly equals poisson rate*time
+# number of input events, nearly roughly poisson rate*time
 Nev = int(freq*SimTime)
 
 # times of Poisson events have exponential distribution
 inpTimes = (exponential(1/freq, Nev) + 2*T).cumsum()/h
+# cutoff events which occur later than simulation time
 inpTimes = array(inpTimes[inpTimes < Tsim - 2*T/h], dtype='int')
 
-# cutoff events which occur later than simulation time
 Nev = len(inpTimes)
 
 # uniformly distributed angles of input events
@@ -96,7 +93,6 @@ for t, theta in zip(inpTimes, inpTheta):
 
 Nreads = linspace(int(N/10), N, 10, dtype='int')
 R = zeros((Tsim, len(Nreads)), dtype='complex')
-
 exactR = zeros(Tsim, dtype='complex')
 
 choises = [[]]*len(Nreads)
@@ -118,7 +114,7 @@ def integrate():
         exactR[t+1] = sum(exp(1j*th)*m)/N
         if CalcMode != 3:
             for idx, Nread in enumerate(Nreads):
-                R[t+1, idx] = R[t, idx] + (-R[t, idx] + sum(exp(1j*th[choises[idx]])*poisson(m[choises[idx]]*h, Nread)))*h/tau_r
+                R[t+1, idx] = R[t, idx] + (-R[t, idx] + (1/Nread)*sum(exp(1j*th[choises[idx]])*poisson(m[choises[idx]]*h, Nread)))*h/tau_r
 
 # error estimation function
 def estimErrDiffNread(lag=0):
@@ -155,39 +151,82 @@ if CalcMode == 0:
     integrate()
 #%%
     # plotting results
-    figure(figsize=(4*4, 3*4))
-    gs = GridSpec(4, 2, height_ratios=[1, 1, 1, 1./3], width_ratios=[30, 1])
+    figure(figsize=(4*2.5, 3*2.5))
+#    gs = GridSpec(4, 2, height_ratios=[1, 1, 2, 1./3], width_ratios=[30, 1])
+    gs = GridSpec(3, 2, height_ratios=[1, 1, 2], width_ratios=[30, 1])
     gs.update(wspace=0.1, hspace=0.1)
     axM = subplot(gs[0, 0])
     axSpec = subplot(gs[1, 0], sharex=axM)
     axAngle = subplot(gs[2, 0], sharex=axM)
-    axEx = subplot(gs[3, 0], sharex=axM)
+#    axEx = subplot(gs[3, 0], sharex=axM)
     axCbar = subplot(gs[:, 1])
 
     egg = axM.pcolormesh(arange(0, SimTime, pltSampl), th*360/(4*pi), ActM.T)
-    spam =colorbar(egg, cax=axCbar)
+    spam = colorbar(egg, cax=axCbar)
+    axCbar.set_title("m[Hz]")
     axM.set_ylim([-90, 90])
     axM.set_ylabel(r"$\theta$")
     setp(axM.get_xticklabels(), visible=False)
     axM.set_title('$U={}\quad I_0={:.2f}$'.format(U, I0))
 
-    axSpec.plot(stime, abs(exactR))
-#    axSpec.plot(stime, abs(R[:, -1]))
+    axSpec.plot(stime, abs(exactR), label='ER')
+    axSpec.plot(stime, abs(R[:, -1]), label='R')
 #    axSpec.plot(arange(0, SimTime, pltSampl), mean(ActM, axis=1))
     setp(axSpec.get_xticklabels(), visible=False)
     axSpec.set_ylabel(r"$|R|$")
+    axSpec.legend(fontsize=16., loc='upper right')
 
-    axAngle.plot(stime, angle(exactR)*360/(4*pi))
-#    axAngle.plot(stime, angle(R[:, -1])*360/(4*pi))
-    axAngle.hlines(inpTheta*360/(4*pi), inpTimes*h, inpTimes*h + T, 'r', linewidth=3)
-    setp(axAngle.get_xticklabels(), visible=False)
+    axAngle.plot(stime, angle(exactR)*360/(4*pi), '-', label='angle(ER)')
+    axAngle.plot(stime, angle(R[:, -1])*360/(4*pi), label='angle(R)')
+    axAngle.hlines(inpTheta*360/(4*pi), inpTimes*h, inpTimes*h + T, 'C3', lw=2.)
+    axAngle.set_ylim([-55, 55])
+    axAngle.legend(fontsize=16., loc='lower right')
+
+#    setp(axAngle.get_xticklabels(), visible=False)
     axAngle.set_ylabel(r"$angle(R)$")
-
-    axEx.plot(stime, Iex, 'r')
-    axEx.set_ylim([0, C + 1])
-    axEx.set_xlim([0, 50])
-    axEx.locator_params(axis = 'y', nbins=3)
-    axEx.set_xlabel(r"$Time [s]$")
+#    axEx.set_xlim([0, SimTime])
+    axAngle.set_xlim((4.59, 5.81))
+    
+#    axEx.plot(stime, Iex, 'r')
+#    axEx.set_ylim([0, C + 1])
+#    axEx.set_yticklabels((0, 20))
+##    axEx.set_xlim([0, SimTime])
+#    axEx.set_xlim((4.59, 5.81))
+#    axEx.locator_params(axis = 'y', nbins=2)
+#    axEx.set_xlabel(r"$Time [s]$")
+    savefig('U_{}.png'.format(U), dpi=260.)
+    #%%
+    figure(figsize=(4*2.3, 3*2.3))
+    plot(stime, angle(exactR)*360/(4*pi), label='exact readout')
+    plot(stime, angle(R[:, -1])*360/(4*pi),  label='sparse readout')
+    hlines(inpTheta*360/(4*pi), inpTimes*h, inpTimes*h + T, 'C3', linewidth=2.)
+    ylim([-90, 90])
+    legend(fontsize=16., loc='upper right')
+    xlim((4.66, 4.86))
+    ylim((-54.0, -19.0))
+    xlabel('Time[s]')
+    ylabel('angle(R)')
+    savefig('angle_U_{}.png'.format(U), dpi=260.)
+    #%%
+#    figure(figsize=(4*2.5, 3*2.5))
+#    gs = GridSpec(2, 2, height_ratios=[1, 1], width_ratios=[30, 1])
+#    gs.update(wspace=0.1, hspace=0.1)
+#    axM = subplot(gs[0, 0])
+#    axSpec = subplot(gs[1, 0], sharex=axM)
+#    axCbar = subplot(gs[:, 1])
+#    axM.set_title('$U={}\quad I_0={:.2f}$'.format(U, I0))
+#
+#    egg = axM.pcolormesh(arange(0, SimTime, pltSampl), th, ActM.T)
+#    spam =colorbar(egg, cax=axCbar)
+#    axCbar.set_title("m[Hz]")
+#    axM.set_ylim([-pi, pi - pi/N])
+#    axM.set_ylabel(r"$\theta$")
+#    setp(axM.get_xticklabels(), visible=False)
+#    axSpec.plot(arange(0, SimTime, pltSampl), mean(ActM, axis=1))
+#    axSpec.set_ylabel(r"$<m>$")
+#    axSpec.set_xlabel(r"$Time [s]$")
+#    axSpec.set_xlim((0, SimTime))
+#    savefig('no_stim_U_{}.png'.format(U), dpi=260.)
 #%%
     errR, errExactR = calcErrDiffNread()
 
@@ -216,7 +255,8 @@ elif CalcMode == 1:
         print("Calculating for U: {}".format(U))
 
         errR, errExactR = calcErrDiffNread()
-        np.save(folderName + 'U_{:.2f}_C_{:.1f}_N_{:n}_SimTime_{:n}.npy'.format(U, C, N, SimTime), errR)
+        save(folderName + 'U_{:.2f}_C_{:.1f}_N_{:n}_SimTime_{:n}.npy'.format(U, C, N, SimTime), errR)
+        save(folderName + 'U_{:.2f}_C_{:.1f}_N_{:n}_SimTime_{:n}_exactPV.npy'.format(U, C, N, SimTime), errExactR)
 elif CalcMode == 2:
     import sys
     U = float(sys.argv[1])
@@ -226,20 +266,20 @@ elif CalcMode == 2:
     integrate()
 
     errR, errExactR = calcErrDiffNread()
-    np.save(folderName + 'U_{:.2f}_C_{:.1f}_N_{:n}_SimTime_{:n}.npy'.format(U, C, N, SimTime), errR)
-    np.save(folderName + 'U_{:.2f}_C_{:.1f}_N_{:n}_SimTime_{:n}_exactPV.npy'.format(U, C, N, SimTime), errExactR)
+    save(folderName + 'U_{:.2f}_C_{:.1f}_N_{:n}_SimTime_{:n}.npy'.format(U, C, N, SimTime), errR)
+    save(folderName + 'U_{:.2f}_C_{:.1f}_N_{:n}_SimTime_{:n}_exactPV.npy'.format(U, C, N, SimTime), errExactR)
 elif CalcMode == 3:
 #    Iex[:] = 0
     fname = 'U_Iex_SimTime_{:.1f}_h_{:.4f}_D_{:.1f}_N_{:n}_eps_{:.3f}_m_{:.1f}.npy'
 
     Eps = .01
-    meanAct = 0.5
+    meanAct = 2.0
 
     Imax = 15.0
     Imin = -15.0
     Ilow, Ihigh = Imin, Imax
 
-    Ierange = zeros_like(Urange) + np.nan
+    Ierange = zeros_like(Urange) + nan
 
     for idx, U in enumerate(Urange):
         for j in xrange(100):
